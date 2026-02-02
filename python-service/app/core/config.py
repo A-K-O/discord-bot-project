2381b0545d18042d031fc9d038c358f05c4ad36b
Aaron <119881692+A-K-O@users.noreply.github.com>
2026-02-01 19:01:30 -0700

    refactored functions for better URIs and querying

---
+++ python-service/app/core/config.py
@@ -0,0 +1,18 @@
+NBA_HEADERS = {
+    'Host': 'stats.nba.com',
+    'Connection': 'keep-alive',
+    'Accept': 'application/json, text/plain, */*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+    'Sec-Ch-Ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+    'Sec-Ch-Ua-Mobile': '?0',
+    'Sec-Ch-Ua-Platform': '"Windows"',
+    'Origin': 'https://www.nba.com',
+    'Sec-Fetch-Site': 'same-site',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Dest': 'empty',
+    'Referer': 'https://www.nba.com/',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
+}
+
+
--- python-service/app/routers/v1/players.py
+++ python-service/app/routers/v1/players.py
@@ -1,13 +1,32 @@
-from fastapi import APIRouter, HTTPException
-from nba_api.stats.static import players
+from fastapi import APIRouter, HTTPException, Query
+from app.services import player_service
+from app.core.enums import PlayerFilterSet
 
 router = APIRouter(prefix="/players", tags=["Players"])
 
 # endpoints
 
-@router.get("/search/{full_name}")
+@router.get("")
+async def get_players(status: PlayerFilterSet = Query(PlayerFilterSet.ALL)):
+    match (status):
+        case PlayerFilterSet.ACTIVE:
+            players_list = player_service.get_all_active_players()
+        case PlayerFilterSet.INACTIVE:
+            players_list = player_service.get_all_inactive_players()
+        case PlayerFilterSet.ALL:
+            players_list = player_service.get_all_players()
+
+    if not players_list:
+        raise HTTPException(status_code=404, detail="Could not return a list of players.")
+
+    return {
+        "count": len(players_list),
+        "results": players_list
+    }
+
+@router.get("{full_name}")
 async def search_player(full_name: str):
-    matching_players = players.find_players_by_full_name(full_name)
+    matching_players = player_service.search_players(full_name)
 
     if not matching_players:
         raise HTTPException(status_code=404, detail="No player found with given name")
